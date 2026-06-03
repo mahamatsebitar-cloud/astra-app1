ec866f218499373dedc1aa99c32e53010a3b30b4
mahamatsebitar-cloud <mahamatsebitar@gmail.com>
2026-06-03 17:54:44 +0100

    insersion revenuecat2

--- android/app/src/main/java/com/nouren/astra/MainActivity.java
+++ android/app/src/main/java/com/nouren/astra/MainActivity.java
@@ -1,5 +1,12 @@
 package com.nouren.astra;
 
 import com.getcapacitor.BridgeActivity;
+import com.revenuecat.purchases.capacitor.PurchasesPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerPlugin(PurchasesPlugin.class);
+    }
+}
